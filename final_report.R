b569fb724faccbcfbb2e19a03fc06c200191b4bd
Urifoz <144878994+Urifoz@users.noreply.github.com>
2023-12-20 15:04:46 +0800

    Update final_report.R

--- final_report.R
+++ final_report.R
@@ -40,3 +40,83 @@ names(summarise2) <-
   split_dg6_2$"15 ~ 19歲"$年度
 
 summarise2
+
+#依年齡分別出託親友師長103~111----
+dg6 |>
+dplyr::filter(
+類別 == "年齡"
+) -> dg6_2
+
+View(dg6_2)
+
+dg6_2 |>
+split(
+dg6_2$項目別
+) -> split_dg6_2
+
+split_dg6_2$"15 ~ 19歲"$託親友師長介紹 -> summarise_1
+split_dg6_2$"20 ~ 24歲"$託親友師長介紹 -> summarise_2
+split_dg6_2$"25 ~ 29歲"$託親友師長介紹 -> summarise_3
+split_dg6_2$"30 ~ 34歲"$託親友師長介紹 -> summarise_4
+split_dg6_2$"35 ~ 39歲"$託親友師長介紹 -> summarise_5
+split_dg6_2$"40 ~ 44歲"$託親友師長介紹 -> summarise_6
+split_dg6_2$"45 ~ 49歲"$託親友師長介紹 -> summarise_7
+split_dg6_2$"50 ~ 54歲"$託親友師長介紹 -> summarise_8
+split_dg6_2$"55 ~ 59歲"$託親友師長介紹 -> summarise_9
+split_dg6_2$"60 ~ 64歲"$託親友師長介紹 -> summarise_10
+split_dg6_2$"65歲及以上"$託親友師長介紹 -> summarise_11
+
+names(summarise_1) <-
+split_dg6_2$"15 ~ 19歲"$年度
+
+summarise_1
+
+names(summarise_2) <-
+split_dg6_2$"20 ~ 24歲"$年度
+
+summarise_2
+
+names(summarise_3) <-
+split_dg6_2$"25 ~ 29歲"$年度
+
+summarise_3
+
+names(summarise_4) <-
+split_dg6_2$"30 ~ 34歲"$年度
+
+summarise_4
+
+names(summarise_5) <-
+split_dg6_2$"35 ~ 39歲"$年度
+
+summarise_5
+
+names(summarise_6) <-
+split_dg6_2$"40 ~ 44歲"$年度
+
+summarise_6
+
+names(summarise_7) <-
+split_dg6_2$"45 ~ 49歲"$年度
+
+summarise_7
+
+names(summarise_8) <-
+split_dg6_2$"50 ~ 54歲"$年度
+
+summarise_8
+
+names(summarise_9) <-
+split_dg6_2$"55 ~ 59歲"$年度
+
+summarise_9
+
+names(summarise_10) <-
+split_dg6_2$"60 ~ 64歲"$年度
+
+summarise_10
+
+names(summarise_11) <-
+split_dg6_2$"65歲及以上"$年度
+
+summarise_11
